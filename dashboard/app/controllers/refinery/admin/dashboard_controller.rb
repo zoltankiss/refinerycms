e9ca7f3cd7115778fe046da4e2784f7086064a02
Zoltan Kiss <zkiss@ackmanndickenson.com>
2014-09-21 23:30:39 -0500

    allow users to create database backups

--- dashboard/app/controllers/refinery/admin/dashboard_controller.rb
+++ dashboard/app/controllers/refinery/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
+require 'yaml_db'
+
 module Refinery
   module Admin
     class DashboardController < Refinery::AdminController
-
       def index
         @recent_inquiries = if Refinery::Plugins.active.find_by_name("refinerycms_inquiries")
           Refinery::Inquiries::Inquiry.latest(Refinery::Dashboard.activity_show_limit)
@@ -10,6 +11,11 @@ module Refinery
         end
       end
 
+      def dump_database
+        path = Rails.root.join('tmp/', "data#{SecureRandom.urlsafe_base64}.yml")
+        SerializationHelper::Base.new(YamlDb::Helper).dump(path)
+        send_data File.open(path).readlines.join("\n"), filename: 'data.txt'
+      end
     end
   end
 end
--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1,5 +1,6 @@
 Refinery::Core::Engine.routes.draw do
   namespace :admin, :path => Refinery::Core.backend_route do
     get 'dashboard', :to => 'dashboard#index', :as => :dashboard
+    get 'dump_database', to: 'dashboard#dump_database'
   end
 end
